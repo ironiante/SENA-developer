59547411ce245017a88dea26ac57699bd6e033f1
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2024-10-18 13:08:30 -0500

    inicio commit

---
+++ conection.php
@@ -0,0 +1,17 @@
+<?php
+
+class connection {
+    private $host = "localhost";
+    private $user = "root"; # name
+    private $pass = ""; # 
+    private $db = "db_sistema";
+    private $conn;
+
+# 2. creacion del metodo constructor.
+
+
+
+
+
+
+
